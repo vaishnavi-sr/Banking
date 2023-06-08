5fcc926c9c689c9d463b329e8dc138d7ccebc994
vaishnavi <vaishu.srini15@gmail.com>
2023-06-08 17:41:09 -0500

    Added variables and methods

--- banking/src/ScannerTest.java
+++ banking/src/ScannerTest.java
@@ -4,16 +4,46 @@ public class ScannerTest {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-//        int a = scanner.nextInt("value = "+ a);
-
-        char c = scanner.next().charAt(0);
-        System.out.println("value = "+c);
-
 
 
     }
 
 
+}
+
+class BankAccount{
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    void deposit(int amount){
+        if(amount != 0){
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount){
+         if(amount != 0){
+             balance = balance - amount;
+             previousTransaction = -amount;
+         }
+
+    }
+
+    void getPreviousTransaction(){
+        if(previousTransaction > 0){
+            System.out.println("Deposited: "+ previousTransaction);
+        }
+        else if(previousTransaction < 0){
+            System.out.println("Withdrawn = "+ Math.abs(previousTransaction));
+        }
+        else{
+            System.out.println("No transaction occured");
+        }
+    }
+    void shoeMenu(){
+
+    }
 }
